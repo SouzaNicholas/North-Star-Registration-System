86e4ca866b4c6f7fd55834c0cc430fa7ea6e9642
SouzaNicholas <76216808+SouzaNicholas@users.noreply.github.com>
2021-11-23 21:28:36 -0500

    Testing branches

--- objects.py
+++ objects.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3 as sql
 
 
 class Student:
@@ -7,15 +7,11 @@ class Student:
         self.ID = ID
         self.name = name
 
-    def add(self):
-        #SQL call to add record to DB
-        #try:
-        #   cursor.execute("""INSERT INTO student (ID, name) VALUES (?, ?)""", (self.ID, self.name))
-        #except Exception as e:
-        #   TODO(Figure out what exception you'd get here)
-        print()
+    def add(self, cursor: sql.Cursor, conn: sql.Connection):
+        cursor.execute("""INSERT INTO student (ID, name) VALUES (?, ?)""", (self.ID, self.name))
+        conn.commit()
 
-    def remove(self):
+    def remove(self, cursor: sql.Cursor, conn: sql.Connection):
         #SQL call to remove record to DB
         #try
         #   cursor.execute()
@@ -23,21 +19,21 @@ class Student:
         #   TODO(Figure out what exception you'd get here)
         print()
 
-    def modify(self, name):
+    def modify(self, name, cursor: sql.Cursor, conn: sql.Connection):
         self.name = name
         #SQL call to update record
         print()
 
-    def add_course(self, ID: str, courseID:str):
+    def add_course(self, ID: str, courseID:str, cursor: sql.Cursor, conn: sql.Connection):
         #SQL call to create enrollment record
         print()
 
 
-    def remove_course(self, ID: str, courseID: str):
+    def remove_course(self, ID: str, courseID: str, cursor: sql.Cursor, conn: sql.Connection):
         #SQL call to delete enrollment record
         print()
 
-    def check_flags(self) -> bool:
+    def check_flags(self, cursor: sql.Cursor, conn: sql.Connection) -> bool:
         #SQL call to total credits among course sections
         #return total > limit
         return False
@@ -49,24 +45,24 @@ class Faculty:
         self.ID = ID
         self.name = name
 
-    def add(self):
+    def add(self, cursor: sql.Cursor, conn: sql.Connection):
         # SQL call to add record to DB
         print()
 
-    def remove(self):
+    def remove(self, cursor: sql.Cursor, conn: sql.Connection):
         #SQL call to remove record to DB
         print()
 
-    def modify(self, name: str):
+    def modify(self, name: str, cursor: sql.Cursor, conn: sql.Connection):
         self.name = name
         #SQL call to update record
         print()
 
-    def add_course(self, ID: str, courseID:str):
+    def add_course(self, ID: str, courseID:str, cursor: sql.Cursor, conn: sql.Connection):
         #SQL call to create enrollment record
         print()
 
-    def remove_course(self, ID: str, courseID: str):
+    def remove_course(self, ID: str, courseID: str, cursor: sql.Cursor, conn: sql.Connection):
         #SQL call to delete enrollment record
         print()
 
@@ -76,10 +72,10 @@ class Course:
     description: str
     credits: int
 
-    def remove(self):
+    def remove(self, cursor: sql.Cursor, conn: sql.Connection):
         print("remove_course")
 
-    def add(self):
+    def add(self, cursor: sql.Cursor, conn: sql.Connection):
         print("add_course")
 
 
@@ -89,15 +85,15 @@ class Section:
     instructor: str
     capacity: int
 
-    def add(self):
+    def add(self, cursor: sql.Cursor, conn: sql.Connection):
         print("add_section")
 
-    def remove(self):
+    def remove(self, cursor: sql.Cursor, conn: sql.Connection):
         print("remove_section")
 
-    def check_flags(self) -> bool:
+    def check_flags(self, cursor: sql.Cursor, conn: sql.Connection) -> bool:
         print("check_flags")
         return False
 
-    def print_class_list(self):
+    def print_class_list(self, cursor: sql.Cursor, conn: sql.Connection):
         print("print_class_list")
