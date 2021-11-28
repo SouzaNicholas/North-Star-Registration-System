d6651f1439f1898c44394850530b97ee24383be6
SouzaNicholas <76216808+SouzaNicholas@users.noreply.github.com>
2021-11-28 15:46:05 -0500

    Performed additional tests on my previous upload. Fixed SQL calls that were not working properly and tested them using the sample database. Added comments and TODOs explaining my thinking.

--- objects.py
+++ objects.py
@@ -7,54 +7,59 @@ class Student:
         self.ID = ID
         self.name = name
 
+    # Adds this student to the database
     def add(self, cursor: sql.Cursor, conn: sql.Connection):
         try:
-            cursor.execute("""INSERT INTO Student (StudentID, Name) VALUES (?, ?)""", (self.ID, self.name))
+            cursor.execute("""INSERT INTO Student (StudentID, Name) VALUES (?, ?);""", (self.ID, self.name))
         except Exception as e:
-            # Display message in GUI along the lines of "Unexpected error occured. Try again."
-            print("placeholder")
+            print("add failed to finish")
         conn.commit()
 
+    # When only one value is needed, string must be passed as a list.
+    # Otherwise, it will be interpreted as a sequence of character inputs.
+    # For example, passing "00001234" will be taken as 8 inputs
     def remove(self, cursor: sql.Cursor, conn: sql.Connection):
         try:
-            cursor.execute("""DELETE FROM Enrollment WHERE 
-                                EXISTS (SELECT * FROM Enrollment WHERE StudentID = (?))""", self.ID)
-            cursor.execute("""DELETE FROM Student WHERE StudentID = (?)""", self.ID)
+            cursor.execute("""DELETE FROM Enrollment WHERE StudentID=(?)""", [self.ID])
         except Exception as e:
-            # Display error message in GUI similar to "Student not found in DB"
-            print("placeholder")
+            print("Could not delete enrollments for", self.ID)
+        try:
+            cursor.execute("""DELETE FROM Student WHERE StudentID = (?)""", [self.ID])
+        except Exception as e:
+            print("remove failed to finish for", self.ID)
         conn.commit()
 
+    # Updates the student's record with the provided name.
     def modify(self, name, cursor: sql.Cursor, conn: sql.Connection):
         self.name = name
         try:
             cursor.execute("""UPDATE Student SET Name=(?) WHERE StudentID=(?)""", (self.name, self.ID))
         except Exception as e:
-            # Display message in GUI along the lines of "Unexpected error occured. Try again."
-            print("placeholder")
+            print("failed to modify student")
         conn.commit()
 
-    def add_course(self, ID: str, course_section_ID:str, cursor: sql.Cursor, conn: sql.Connection):
+    # Creates an enrollment record linking this student to the provided seciton record
+    def add_course(self, course_section_ID:str, cursor: sql.Cursor, conn: sql.Connection):
         try:
-            cursor.execute("""INSERT INTO Enrollment (No., StudentID, Course_SectionID, Flag) VALUES (default, ?, ?, ?)""",
-                       (ID, course_section_ID, 0))  # Passes default to No., as it's auto incremented
+            cursor.execute("""INSERT INTO Enrollment (StudentID, Course_SectionID, Flag) VALUES (?, ?, ?)""",
+                           (self.ID, course_section_ID, 0))  # Passes nothing to EnrollmentID, as it's auto incremented
         except Exception as e:
-            # Display message in GUI along the lines of "Unexpected error occured. Try again."
-            print("placeholder")
+            print("Could not add course")
         conn.commit()
 
+    # Deletes an enrollment record connecting this student and the provided section.
     def remove_course(self, course_section_ID: str, cursor: sql.Cursor, conn: sql.Connection):
         try:
-            cursor.execute("""DELETE FROM Enrollment WHERE EXISTS 
-                      (SELECT * FROM Enrollment WHERE StudentID = (?) AND COURSE_SECTIONID = (?))""",
-                       (self.ID, course_section_ID))
+            cursor.execute("""DELETE FROM Enrollment WHERE StudentID = (?) AND COURSE_SECTIONID = (?)""",
+                           (self.ID, course_section_ID))
         except Exception as e:
-            # Display message in GUI along the lines of "Unexpected error occured. Try again."
-            print("placeholder")
+            print("Could not remove course")
         conn.commit()
 
+    # TODO: This requires a complicated series of SQL calls that I will return to. -NS
     def check_flags(self, cursor: sql.Cursor, conn: sql.Connection) -> bool:
-        #SQL call to total credits among course sections
+        # SQL call to find credit value of student's courses
+        cursor.execute("""""")
         #return total > limit
         return False
 
@@ -65,35 +70,48 @@ class Faculty:
         self.ID = ID
         self.name = name
 
+    # Adds the faculty record to the database.
     def add(self, cursor: sql.Cursor, conn: sql.Connection):
         try:
             cursor.execute("""INSERT INTO Faculty (FacultyID, Name) VALUES (?, ?)""", (self.ID, self.name))
         except Exception as e:
             # Display message in GUI along the lines of "Unexpected error occured. Try again."
-            print("placeholder")
+            print("Failed to add faculty")
         conn.commit()
 
+    # Checks to make sure the faculty member is not teaching any courses.
+    # If they are not, they will be removed form the database.
     def remove(self, cursor: sql.Cursor, conn: sql.Connection):
-        # Must be implemented in such a way that it checks Section to make sure
-        # The faculty member has no dependencies. If so, the record will be deleted
-        # Will revisit later -NS
+        cursor.execute("""SELECT * FROM Section WHERE FacultyID = (?)""", [self.ID])
+        if len(cursor.fetchall()) < 1:
+            try:
+                cursor.execute("""DELETE FROM Faculty WHERE FacultyID = (?) AND FacultyID = (?)""", (self.ID, self.ID))
+            except Exception as e:
+                # Display error message in GUI similar to "Student not found in DB"
+                print("remove failed to finish for", self.ID)
+        else:
+            print("Could not delete faculty. Remove from current courses.")
         conn.commit()
 
+    # Updates a faculty record with the provided name.
     def modify(self, name: str, cursor: sql.Cursor, conn: sql.Connection):
         self.name = name
         try:
             cursor.execute("""UPDATE Faculty SET Name=(?) WHERE FacultyID=(?)""", (self.name, self.ID))
         except Exception as e:
             # Display message in GUI along the lines of "Unexpected error occured. Try again."
-            print("placeholder")
+            print("Failed to update faculty:", self.ID)
         conn.commit()
 
+    # Changes the listed faculty ID on the provided Section record.
+    # TODO: This method is currently able to insert IDs of faculty members that do not exist. Will fix. -NS
     def add_course(self, course_section_ID:str, cursor: sql.Cursor, conn: sql.Connection):
         try:
-            cursor.execute("""UPDATE Section SET FacultyID=(?) WHERE Course_SectionID=(?)""", (self.ID, course_section_ID))
+            cursor.execute("""UPDATE Section SET FacultyID=(?) WHERE Course_SectionID=(?)""",
+                           (self.ID, course_section_ID))
         except Exception as e:
             # Display message in GUI along the lines of "Unexpected error occured. Try again."
-            print("placeholder")
+            print("Failed to add faculty to course for:", self.ID)
         conn.commit()
 
     def remove_course(self, ID: str, courseID: str, cursor: sql.Cursor, conn: sql.Connection):
